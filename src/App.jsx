2eeb215a9e19dd2eb896bbc591f9f1d1582a882d
Ronakp08 <ronakp6393@gmail.com>
2024-12-05 17:02:20 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import Header from "./components/Layouts/Header.jsx";
-import Loader from "./Components/Loader.jsx";
+// import Loader from "./Components/Loader.jsx";
 import Footer from "./components/Layouts/Footer.jsx"
 import Contact from "./components/Contact.jsx";
 
 function App() {
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000); // 2 seconds delay to simulate loading
-    return () => clearTimeout(timer); // Cleanup
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setLoading(false);
+  //   }, 2000); // 2 seconds delay to simulate loading
+  //   return () => clearTimeout(timer); // Cleanup
+  // }, []);
 
-  if (loading) {
-    return <Loader />; // Show the loader when loading is true
-  }
+  // if (loading) {
+  //   return <Loader />; // Show the loader when loading is true
+  // }
 
   return (
     <Router>
